2a4e5c5abb9dfe3aae0ac0f53aaec5abe8954dca
Shlok Tiwari <shlokt16@gmail.com>
2026-06-19 23:58:28 +0530

    project part

---
+++ youtube_management.py
@@ -0,0 +1,11 @@
+
+
+
+while true:
+    print("/Open a youtube video")
+    print("1.insert all youtube videos")
+    print("2add a youtube video")
+    print("3.update a youtube video")
+    print("4.delete a youtube video")
+    print("5.exit the app")
+    Choice = input("enter your choice")
